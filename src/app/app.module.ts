1cf1f598eeebc1a16e7a0356e58f757ee06484e9
Bhavik Patel <bhavik.v.patel@gmail.com>
2017-08-17 14:15:07 +0530

    Initial CV Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PortfolioComponent,
+    BlogComponent,
+    ContactComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
